e04adbe25ffaebdd30f731be61addca5e213a86d
Lilithke <bocsi.vi13@gmail.com>
2023-07-31 00:42:36 +0200

    https://github.com/Lilithke/07_27.git

--- HegyekMO_Évi.py
+++ HegyekMO_Évi.py
@@ -4,6 +4,8 @@ class Hegyek:
     self.hegy_csucs=hegy_csucs
     self.hegyseg=hegyseg
     self.magassag=magassag
+ def labban(self):
+     return self.magassag*3.280839895
  
 lista=[]
 def beolvasas():
@@ -31,13 +33,78 @@ def F4():
         ossz+=i.magassag
     atlag=ossz/len(lista)
     print(f"4.feladat: Hegycsúcsok átlagos magassága: {atlag} m")
+
+def F5():
+    leg=lista[0]
+    for i in lista:
+        if leg.magassag<i.magassag:
+            leg=i
+        
+        
+    print(f"5.feladat: A legmagassabb hegycsúcs adatai:")
+    print(f"\tnev:{leg.hegy_csucs}")
+    print(f"\thegyseg:{leg.hegyseg}")
+    print(f"\tmagassag:{leg.magassag} m")
+
+def F6():
+    szam=int(input("6.feladat: Kérek egy magassagot: "))
+    
+    for i in lista:
+        if i.hegyseg=="Börzsöny" and i.magassag > szam:
+            print(f"\tVan {szam}m-nél magasabb hegycsúcs a Börzsönyben!")
+            break
+    else:
+            print(f"\tNincs {szam}m-nél magasabb hegycsúcs a Börzsönyben!")
+
+def F7():
+    db = 0
+    for i in lista:
+        if i.labban()>3000:
+            db+=1
+    print(f"7.feladat: 3000 lábnál magassabb hegycsúcsok száma:{db}")
+
+def F8():
+    db1=0
+    db2=0
+    db3=0
+    db4=0
+    db5=0
+    for i in lista:
+        if i.hegyseg=="Mátra":
+            db1+=1
+            
+        elif i.hegyseg=="Bükk-vidék":
+            db2+=1
+        
+        elif i.hegyseg=="Börzsöny":
+            db3+=1
+
+        elif i.hegyseg=="Zempléni-hegység":
+            db4+=1
+        elif i.hegyseg=="Kőszegi-hegység":
+            db5+=1
+    print(f"8.feladat: Hegység statisztika")
+    print(f"\t Mátra - {db1} db")
+    print(f"\t Bükk-vidék - {db2} db")
+    print(f"\t Börzsöny- {db3} db")
+    print(f"\t Zempléni-hegység - {db4} db")
+    print(f"\t Kőszegi-hegység - {db5} db")
+
+
+    
+
+
        
   
 
 def main():
      beolvasas()
-     #kiir()
+     kiir()
      F3()
      F4()
+     F5()
+     F6()
+     F7()
+     F8()
     
 main()
